ef4c70b60118cb340cf9ed5bba847d4b52d224c8
Roberto Tadeu <celulartadeu@gmail.com>
2026-05-24 20:58:08 +0100

    Sistema IA TVDE inicial

---
+++ app.py
@@ -0,0 +1,10 @@
+print("=== ASSISTENTE IA TVDE ===")
+
+nome = input("Digite seu nome: ")
+ganho = input("Quanto ganhou hoje no TVDE? ")
+
+print("")
+print("Motorista:", nome)
+print("Faturamento do dia:", ganho, "euros")
+cidade = input("Cidade de operação: ")
+print("Análise IA: Continue consistente 🚀")
